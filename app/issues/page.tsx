b62dc556b5af49226f5ed3fc762abf01552ed699
Malay Pandit <malay.pandit@dinereel.com>
2024-07-07 14:02:23 +0530

    added edit and delete api

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import { IssueModel } from "../model/IssueModel";
-import axios from "axios";
 
 const IssuePage = () => {
   return (
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -4,3 +4,9 @@ export const createIssueSchema = z.object({
     title: z.string().min(1, 'Title is required!').max(255),
     description: z.string().min(1, 'Description is required!')
 })
+
+export const updateIssueSchema = z.object({
+    id: z.number(),
+    title: z.string().optional(),
+    description: z.string().optional(),
+});
